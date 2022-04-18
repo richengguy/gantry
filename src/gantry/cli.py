0e728c230e28da1a4f3ee5e82630b293b65a288f
richengguy <richengguy@users.noreply.github.com>
2022-04-17 21:35:28 -0400

    Enable gantry to support JSON schema exports. (#4)

--- src/gantry/cli.py
+++ src/gantry/cli.py
@@ -1,13 +1,21 @@
+import json
 from pathlib import Path
 
 import click
 
-from . import ServiceGroupDefinition
+from . import ServiceGroupDefinition, __version__
 from .build import build_services
 from .exceptions import ComposeServiceBuildError, InvalidServiceDefinitionError
+from .schemas import get_schema, Schema
 
 
-@click.command()
+@click.group()
+@click.version_option(version=__version__)
+def main():
+    '''Manage containers on single-host systems.'''
+
+
+@main.command()
 @click.option('--services', '-s', default='./services',
               help='Folder containing the service definitions. Default is \'./services\'.',
               type=click.Path(exists=True, file_okay=False, dir_okay=True, path_type=Path))
@@ -16,8 +24,8 @@ from .exceptions import ComposeServiceBuildError, InvalidServiceDefinitionError
               type=click.Path(exists=False, file_okay=False, dir_okay=True, path_type=Path))
 @click.option('--force', '-f', 'force_overwrite', is_flag=True,
               help='Allow an existing output folder to be overwritten.')
-@click.version_option()
-def main(services: Path, output: Path, force_overwrite: bool):
+def build_compose(services: Path, output: Path, force_overwrite: bool):
+    '''Build a Docker Compose specification from a service group.'''
     try:
         service_group = ServiceGroupDefinition(services)
     except InvalidServiceDefinitionError as e:
@@ -31,6 +39,44 @@ def main(services: Path, output: Path, force_overwrite: bool):
         build_services(service_group, output, overwrite=force_overwrite)
     except ComposeServiceBuildError as e:
         raise click.ClickException(str(e))
+
+
+@main.group()
+def schemas():
+    '''Access the schemas that validate service groups and definitions.'''
+
+
+@schemas.command('dump')
+@click.argument('name', metavar='NAME',
+                type=click.Choice([schema.value for schema in Schema]))
+def schemas_dump(name: str):
+    '''Prints a schema in JSON format to stdout.
+
+    The list of valid schema names can be found with `gantry schemas list`.
+    '''
+    schema = Schema(name)
+    contents = get_schema(schema)
+    click.echo(json.dumps(contents, indent=4))
+
+
+@schemas.command('export')
+@click.option('--output', '-o', default='./schemas',
+              type=click.Path(file_okay=False, dir_okay=True, path_type=Path),
+              help='Output folder for the schema files.  Defaults to \'./schemas\'.')
+def schemas_export(output: Path):
+    '''Export the JSON schema files used by gantry.'''
+    output.mkdir(exist_ok=True)
+    for schema in Schema:
+        contents = get_schema(schema)
+        with (output / f'{schema.value}.json').open('wt') as f:
+            json.dump(contents, f, indent=4)
+
+
+@schemas.command('list')
+def schemas_list():
+    '''List the schemas used by gantry.'''
+    for schema in Schema:
+        click.echo(schema.value)
 
 
 if __name__ == '__main__':
---
+++ test/test_schema_export.py
@@ -0,0 +1,51 @@
+import json
+from pathlib import Path
+
+from click.testing import CliRunner
+
+from gantry import cli
+from gantry.schemas import Schema, get_schema
+
+import pytest
+
+
+@pytest.mark.parametrize('schema', [Schema.SERVICE, Schema.SERVICE_GROUP])
+def test_schema_dump(schema: Schema):
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ['schemas', 'dump', schema.value])
+    assert result.exit_code == 0
+
+    # Dumped JSON schema and directly loaded schema should be identical.
+    contents = get_schema(schema)
+    parsed = json.loads(result.output)
+    assert contents == parsed
+
+
+@pytest.mark.parametrize('schema', [Schema.SERVICE, Schema.SERVICE_GROUP])
+def test_schema_export(schema: Schema, tmp_path: Path):
+    # NOTE: Using parameterization to make it easier to isolate issues by
+    # checking each schema type separately.
+
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=tmp_path) as td:
+        result = runner.invoke(cli.main, ['schemas', 'export'])
+        assert result.exit_code == 0
+
+        # Exported JSON file and directly loaded schema should be identical.
+        json_schema = Path(td) / 'schemas' / f'{schema.value}.json'
+        with json_schema.open('rt') as f:
+            parsed = json.load(f)
+
+        contents = get_schema(schema)
+        assert contents == parsed
+
+
+def test_schema_list():
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ['schemas', 'list'])
+    assert result.exit_code == 0
+
+    schemas = result.output.splitlines()
+    assert len(schemas) == 2
+    assert schemas[0] == Schema.SERVICE.value
+    assert schemas[1] == Schema.SERVICE_GROUP.value
