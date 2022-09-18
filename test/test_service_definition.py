372dd05d12bcaa4ae4558f5e69b5a5753db57a55
Richard Rzeszutek <richengguy@users.noreply.github.com>
2022-09-17 22:23:50 -0400

    Got tests setup.

--- test/test_service_definition.py
+++ test/test_service_definition.py
@@ -13,9 +13,12 @@ def _get_build_args(compose_spec: ComposeFile, service: str) -> dict[str, str | 
     return compose_spec['services'][service]['build'].get('args', {})
 
 
-def _get_routing_rule(compose_spec: ComposeFile, service: str) -> tuple[str, int | None]:
+def _get_routing_rule(compose_spec: ComposeFile, service: str) -> tuple[set[str], int | None]:
     labels = compose_spec['services'][service]['labels']
-    path_rule = labels[f'traefik.http.routers.{service}.rule']
+    path_rule = set(
+        rule.strip()
+        for rule in labels[f'traefik.http.routers.{service}.rule'].split('||')
+    )
     port_rule = labels.get(f'traefik.http.services.{service}.loadbalancer.server.port')
     return (path_rule, port_rule)
 
@@ -23,12 +26,12 @@ def _get_routing_rule(compose_spec: ComposeFile, service: str) -> tuple[str, int
 @pytest.mark.parametrize(
     ('service', 'expected_rule', 'expected_port'),
     [
-        ('complex-entrypoint', 'PathPrefix(`/foo`) || PathPrefix(`/bar`)', 8080),
-        ('default', 'PathPrefix(`/default`)', 80),
-        ('string-entrypoint', 'PathPrefix(`/my-service`)', 80)
+        ('complex-entrypoint', {'PathPrefix(`/foo`)', 'PathPrefix(`/bar`)'}, 8080),
+        ('default', {'PathPrefix(`/default`)'}, 80),
+        ('string-entrypoint', {'PathPrefix(`/my-service`)'}, 80)
     ]
 )
-def test_entrypoints(service: str, expected_rule: str, expected_port: str,
+def test_entrypoints(service: str, expected_rule: set[str], expected_port: str,
                      compile_compose_file: CompileFn):
     compose_file = compile_compose_file('service-definition', 'entrypoints')
     path_rule, port_rule = _get_routing_rule(compose_file, service)
@@ -45,6 +48,6 @@ def test_entrypoints(service: str, expected_rule: str, expected_port: str,
 )
 def test_build_args(service: str, expected_args: dict[str, str | int],
                     compile_compose_file: CompileFn):
-    compose_file = compile_compose_file('service_definition', 'build-args')
+    compose_file = compile_compose_file('service-definition', 'build-args')
     build_args = _get_build_args(compose_file, service)
     assert build_args == expected_args
