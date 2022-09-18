b9bb9bbecd1d7830bdb1d0cf0a0cb2623404bf3b
Richard Rzeszutek <richengguy@users.noreply.github.com>
2022-09-17 21:38:52 -0400

    Added a "complex entrypoint" test.

--- test/test_service_definition.py
+++ test/test_service_definition.py
@@ -3,8 +3,21 @@ from typing import Callable
 from gantry._compose_spec import ComposeFile
 
 
+def _get_routing_rule(compose_spec: ComposeFile, service: str) -> tuple[str, int | None]:
+    labels = compose_spec['services'][service]['labels']
+    path_rule = labels[f'traefik.http.routers.{service}.rule']
+    port_rule = labels.get(f'traefik.http.services.{service}.loadbalancer.server.port')
+    return (path_rule, port_rule)
+
+
+def test_complex_entrypoint(compile_compose_file: Callable[[str, str], ComposeFile]):
+    compose_spec = compile_compose_file('service-definition', 'entrypoints')
+    path_rule, port_rule = _get_routing_rule(compose_spec, 'complex-entrypoint')
+    assert path_rule == 'PathPrefix(`/foo`) || PathPrefix(`/bar`)'
+    assert port_rule == 8080
+
+
 def test_string_entrypoint(compile_compose_file: Callable[[str, str], ComposeFile]):
     compose_sec = compile_compose_file('service-definition', 'entrypoints')
-    labels = compose_sec['services']['string-entrypoint']['labels']
-    http_rule = labels['traefik.http.routers.string-entrypoint.rule']
-    assert http_rule == 'PathPrefix(\'/my-service\')'
+    path_rule, _ = _get_routing_rule(compose_sec, 'string-entrypoint')
+    assert path_rule == 'PathPrefix(`/my-service`)'
