ae05c22c9cd9dc93f498f1395d8b512c58893b2b
Richard Rzeszutek <richengguy@users.noreply.github.com>
2022-09-17 21:23:15 -0400

    Renamed test.

--- test/test_service_definition.py
+++ test/test_service_definition.py
@@ -4,7 +4,7 @@ from gantry._compose_spec import ComposeFile
 
 
 def test_string_entrypoint(compile_compose_file: Callable[[str, str], ComposeFile]):
-    compose_sec = compile_compose_file('service-definition', 'string-entrypoint')
+    compose_sec = compile_compose_file('service-definition', 'entrypoints')
     labels = compose_sec['services']['string-entrypoint']['labels']
     http_rule = labels['traefik.http.routers.string-entrypoint.rule']
     assert http_rule == 'PathPrefix(\'/my-service\')'
