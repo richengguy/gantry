57b5b7a517eb6c5c41866b890a1d54b519ad1afd
Richard Rzeszutek <richengguy@users.noreply.github.com>
2022-09-25 21:09:41 -0400

    Added a new test.
    This looks for whether or not the traefik configuration is customized
    and rendered correctly.

--- test/test_traefik_router.py
+++ test/test_traefik_router.py
@@ -1,8 +1,14 @@
+from pathlib import Path
 from typing import Callable
 
 from gantry._compose_spec import ComposeFile
 
+from ruamel.yaml import YAML
+
 import pytest
+
+CompileFn = Callable[[str, str], ComposeFile]
+ServicesFn = Callable[[str, str], Path]
 
 
 @pytest.mark.parametrize(
@@ -18,3 +24,25 @@ def test_router_args(sample: str, expected: str,
     compose_spec = compile_compose_file('router', sample)
     volumes = compose_spec['services']['proxy']['volumes']
     assert volumes[0] == f'{expected}:/var/run/docker.sock:ro'
+
+
+def test_router_config_render(compile_services: ServicesFn):
+    '''Check that the router's configuration is rendered correctly.'''
+    output_path = compile_services('router', 'traefik-custom-config')
+    assert (output_path / 'traefik-custom.yml').exists()
+
+    # Check that the traefik file sets the network correctly.
+    reader = YAML()
+    with (output_path / 'traefik-custom.yml').open('rt') as f:
+        traefik_file = reader.load(f)
+
+    network = traefik_file['providers']['docker']['network']
+    assert network == 'test'
+
+    # Check that the compose file references the correct traefik file.
+    reader = YAML()
+    with (output_path / 'docker-compose.yml').open('rt') as f:
+        compose_spec: ComposeFile = reader.load(f)
+
+    volumes = compose_spec['services']['proxy']['volumes']
+    assert volumes[1] == './traefik-custom.yml:/traefik.yml:ro'
