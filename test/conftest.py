80f58f7a28019a8577ba2240377cdfdeb2df435f
richengguy <richengguy@users.noreply.github.com>
2022-09-26 20:39:40 -0400

    No longer using a hard-coded file name for the Traefik configuration file. (#10)

--- test/conftest.py
+++ test/conftest.py
@@ -18,18 +18,37 @@ def compile_compose_file(samples_folder: Path, tmp_path: Path) -> Callable[[str,
 
     def _run_command(folder: str, sample: str) -> ComposeFile:
         runner = CliRunner()
-        default_example = samples_folder / folder / sample
+        sample_path = samples_folder / folder / sample
         with runner.isolated_filesystem(temp_dir=tmp_path) as td:
-            result = runner.invoke(cli.main, ['build-compose', '-s', default_example.as_posix()])
+            result = runner.invoke(cli.main, ['build-compose', '-s', sample_path.as_posix()])
             compose_file = Path(td) / 'services.docker' / 'docker-compose.yml'
 
             click.echo(result.stdout)
             assert result.exit_code == 0
-            assert compose_file.exists
+            assert compose_file.exists()
 
             yaml = YAML()
             with compose_file.open('rt') as f:
                 return yaml.load(f)
+
+    return _run_command
+
+
+@pytest.fixture()
+def compile_services(samples_folder: Path, tmp_path: Path) -> Callable[[str, str], Path]:
+
+    def _run_command(folder: str, sample: str) -> Path:
+        runner = CliRunner()
+        sample_path = samples_folder / folder / sample
+        with runner.isolated_filesystem(temp_dir=tmp_path) as td:
+            result = runner.invoke(cli.main, ['build-compose', '-s', sample_path.as_posix()])
+            output_path = Path(td) / 'services.docker'
+
+            click.echo(result.stdout)
+            assert result.exit_code == 0
+            assert (output_path / 'docker-compose.yml').exists()
+
+            return output_path
 
     return _run_command
 
