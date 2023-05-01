c2678b37c71fb5e76c96124402451176f4cd2aed
Richard Rzeszutek <richengguy@users.noreply.github.com>
2023-04-30 22:05:31 -0400

    Fixing issue in build script.

--- samples/simple-tls/_build.sh
+++ samples/simple-tls/_build.sh
@@ -4,5 +4,5 @@ gantry build-compose -s $SAMPLE_FOLDER -o $OUTPUT_FOLDER
 
 # Build the sample docker images and ensure all dependencies can be pulled down.
 cd $OUTPUT_FOLDER
-docker compose build
 docker compose pull
+docker compose build
