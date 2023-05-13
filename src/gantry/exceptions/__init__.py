6020e79432bc8bd707385db6451c2838a8b02912
Richard Rzeszutek <richengguy@users.noreply.github.com>
2023-05-13 11:53:06 -0400

    Started adding in new exceptions.

--- src/gantry/exceptions/__init__.py
+++ src/gantry/exceptions/__init__.py
@@ -4,6 +4,12 @@ from .config import (
     InvalidConfigValueError,
 )
 
+from .images import (
+    ImageTargetException,
+    ClientConnectionError,
+    ServiceImageBuildError,
+)
+
 from .services import (
     ServiceManagerException,
     ComposeServiceBuildError,
