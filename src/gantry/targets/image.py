621cb4a4e19299f1f481823619821c595c953cdf
Richard Rzeszutek <richengguy@users.noreply.github.com>
2023-05-12 23:54:28 -0400

    Small initializer updates.

--- src/gantry/targets/image.py
+++ src/gantry/targets/image.py
@@ -11,21 +11,33 @@ _logger = get_app_logger('build-image')
 
 class ImageTarget(Target):
     '''Build the container images for a service group.'''
-    def __init__(self, output: PathLike) -> None:
+    def __init__(self, registry: str, tag: str, build_folder: PathLike) -> None:
+        '''
+        Parameters
+        ----------
+        registry : str
+            name of the registry the images should be pushed to
+        tag : str
+            the image tag; often this will be the build verison
+        build_folder : path-like
+            path to the build folder when building the images
+        '''
         super().__init__()
-        self._output = Path(output)
+        self._build_folder = Path(build_folder)
+        self._registry = registry
+        self._tag = tag
 
         self._pipeline = Pipeline(stages=[
-            CopyServiceResources(self._output)
+            CopyServiceResources(self._build_folder)
         ])
 
     def build(self, service_group: ServiceGroupDefinition) -> None:
         _logger.debug('Building images for service group.')
 
-        if self._output.exists():
-            _logger.debug('Removing existing build directory \'%s\'.', self._output)
-            shutil.rmtree(self._output)
-            self._output.rmdir()
+        if self._build_folder.exists():
+            _logger.debug('Removing existing build directory \'%s\'.', self._build_folder)
+            shutil.rmtree(self._build_folder)
+            self._build_folder.rmdir()
 
-        self._output.mkdir(parents=False)
+        self._build_folder.mkdir(parents=False)
         self._pipeline.run(service_group)
