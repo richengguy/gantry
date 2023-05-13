036bebf06c3b72c9967e68ec94a03cccebdfa0a0
Richard Rzeszutek <richengguy@users.noreply.github.com>
2023-05-13 00:28:21 -0400

    More coding.

--- src/gantry/targets/image.py
+++ src/gantry/targets/image.py
@@ -1,12 +1,42 @@
 import shutil
 
+import docker
+from docker.errors import APIError
+
 from ._common import CopyServiceResources, Pipeline, Target
 
 from .._types import Path, PathLike, get_app_logger
-from ..services import ServiceGroupDefinition
+from ..services import ServiceDefinition, ServiceGroupDefinition
 
 
 _logger = get_app_logger('build-image')
+
+
+class _ImageBuilder:
+    def __init__(self, folder: Path, registry: str, tag: str) -> None:
+        try:
+            self._api = docker.from_env()
+        except APIError as e:
+            _logger.critical('Failed to create Docker API client.', exc_info=e)
+            raise e
+
+        self._folder = folder
+        self._registry = registry
+        self._tag = tag
+
+    def build(self, service: ServiceDefinition) -> None:
+        image_name = f'{self._registry}/{service.name}:{self._tag}'
+        _logger.debug('Building image %s', image_name)
+
+
+class BuildDockerImages:
+    '''Build Docker images for each service in a service group.'''
+    def __init__(self, build_folder: Path, registry: str, tag: str) -> None:
+        self._builder = _ImageBuilder(build_folder, registry, tag)
+
+    def run(self, service_group: ServiceGroupDefinition) -> None:
+        for service in service_group:
+            self._builder.build(service)
 
 
 class ImageTarget(Target):
