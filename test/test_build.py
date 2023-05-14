6e7c208d802f7e37ea1568b84dc87f5a15f23fe9
Richard Rzeszutek <richengguy@users.noreply.github.com>
2023-05-14 01:18:13 -0400

    Fixed a test error.

--- test/test_build.py
+++ test/test_build.py
@@ -1,10 +1,14 @@
+from pathlib import Path
+
 from click.testing import CliRunner
 
 from gantry import cli
 
 
-def test_cannot_provide_both_tag_and_build_id() -> None:
+def test_cannot_provide_both_tag_and_build_id(samples_folder: Path) -> None:
+    path = samples_folder / 'service-definition' / 'build-args'
+
     runner = CliRunner()
-    result = runner.invoke(cli.main, ['build', '-t', '1', '-n', '1'])
+    result = runner.invoke(cli.main, ['build', '-t', '1', '-n', '1', str(path)])
     assert result.exit_code != 0
     assert 'Cannot specify both "--tag" and "--build-number".' in result.stdout
