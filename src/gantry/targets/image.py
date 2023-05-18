af266f5601ab0b845c92e1faab21c1a033081c95
Richard Rzeszutek <richengguy@users.noreply.github.com>
2023-05-17 23:41:28 -0400

    Got the logging to work properly.

--- src/gantry/targets/image.py
+++ src/gantry/targets/image.py
@@ -1,12 +1,21 @@
 import json
 import shutil
-from typing import Generator
+from typing import Iterator
 
 import docker
 from docker.constants import DEFAULT_UNIX_SOCKET
 from docker.errors import DockerException
 
-from rich.progress import Progress, SpinnerColumn, track
+from rich.console import Group
+from rich.live import Live
+from rich.progress import (
+    BarColumn,
+    Progress,
+    TaskID,
+    TextColumn,
+    TimeElapsedColumn
+)
+from rich.table import Table
 
 from ._common import CopyServiceResources, Pipeline, Target
 
@@ -17,6 +26,94 @@ from ..services import ServiceDefinition, ServiceGroupDefinition
 
 
 _logger = get_app_logger('build-image')
+
+
+class _BuildStatus:
+    '''Handle reporting the current build status.'''
+
+    class _ContextWrapper:
+        def __init__(self, image: str, build_status: '_BuildStatus') -> None:
+            self._build_status = build_status
+            self._image = image
+
+        def __enter__(self) -> '_BuildStatus':
+            self._build_status._build_started(self._image)
+            return self._build_status
+
+        def __exit__(self, exc_type, exc_val, exc_tb) -> None:
+            self._build_status._build_complete()
+
+    def __init__(self, build_progress: Progress, build_output: Table) -> None:
+        self._build_output = build_output
+        self._build_progress = build_progress
+        self._task_id = TaskID(-1)
+
+    def _build_started(self, image: str) -> None:
+        self._task_id = self._build_progress.add_task('', image=image, start=True, total=None)
+
+    def _build_complete(self) -> None:
+        self._build_progress.update(self._task_id, total=1, completed=True)
+        self._build_progress.stop_task(self._task_id)
+
+    def report_start(self, image: str) -> '_ContextWrapper':
+        return _BuildStatus._ContextWrapper(image, self)
+
+    def log_output(self, log: str) -> None:
+        _logger.debug('<<Docker>> %s', log)
+        self._build_output.add_row(log)
+        self._build_progress.update(self._task_id)
+
+
+class _OverallProcessingStatus:
+    '''Setup the internal renderer to show the overall processing status.'''
+
+    def __init__(self, service_group: ServiceGroupDefinition) -> None:
+        self._service_group = service_group
+
+        self._total_progress = Progress(
+            TimeElapsedColumn(),
+            TextColumn('Building Services'),
+            BarColumn()
+        )
+
+        self._stage_progress = Progress(
+            TimeElapsedColumn(),
+            TextColumn('Image: [bold blue]{task.fields[image]}[/bold blue]'),
+            BarColumn()
+        )
+
+        self._build_log = _OverallProcessingStatus._create_log()
+
+    def _create_renderable_group(self) -> Group:
+        self._build_log = _OverallProcessingStatus._create_log()
+        return Group(
+            self._total_progress,
+            self._stage_progress,
+            self._build_log,
+        )
+
+    def get_build_status_reporter(self) -> _BuildStatus:
+        return _BuildStatus(self._stage_progress, self._build_log)
+
+    @property
+    def services(self) -> Iterator[ServiceDefinition]:
+        task_id = self._total_progress.add_task('', start=True, total=len(self._service_group))
+
+        with Live() as renderable:
+            renderable.update(self._create_renderable_group(), refresh=True)
+
+            for service in self._service_group:
+                yield service
+                self._total_progress.update(task_id, advance=1)
+                renderable.update(self._create_renderable_group(), refresh=True)
+
+        self._total_progress.update(task_id, completed=True)
+
+    @staticmethod
+    def _create_log() -> Table:
+        table = Table.grid()
+        table.add_column()
+        return table
 
 
 class _ImageBuilder:
@@ -32,38 +129,26 @@ class _ImageBuilder:
         self._registry = registry
         self._tag = tag
 
-    def build(self, service: ServiceDefinition) -> None:
+    def build(self, service: ServiceDefinition, build_status: _BuildStatus) -> None:
         image_name = f'{self._registry}/{service.name}:{self._tag}'
         dockerfile_folder = (self._folder / service.name).absolute().as_posix()
 
         _logger.debug('Building image \'%s\' from \'%s\'', image_name, dockerfile_folder)
 
-        # api_response: Generator[bytes, None, None] = self._api.build(path=dockerfile_folder,
-        #                                                              tag=image_name,
-        #                                                              rm=True)
+        response: Iterator[bytes] = self._api.build(path=dockerfile_folder, tag=image_name, rm=True)
 
-        with Progress(SpinnerColumn(), transient=True) as progress:
-            progress.add_task(f'Build {service.name}', start=False)
-            import time
-            for i in range(3):
-                time.sleep(1)
+        # The API reponse is JSON formatted with *multiple* JSON objects being
+        # returned in the same newline-terminated string.  The code below is
+        # just splitting up the reponse into its individual lines and then
+        # logging them if the object contains a 'stream' property.
 
-            # for output in api_response:
-            #     lines = output.splitlines()
-            #     for line in lines:
-            #         obj: dict[str, str] = json.loads(line)
-            #         if text := obj.get('stream'):
-            #             progress.log(text.strip(), highlight=False)
-            progress.stop()
-
-        # output: bytes
-        # for output in self._api.build(path=dockerfile_folder, tag=image_name, rm=True):
-        #     parts = output.splitlines()
-        #     _console.log(output)
-        #     for part in parts:
-        #         obj: dict[str, str] = json.loads(part)
-        #         if text := obj.get('stream'):
-        #             _console.log(text.strip(), highlight=False)
+        with build_status.report_start(image_name) as reporter:
+            for segment in response:
+                lines = segment.splitlines()
+                for line in lines:
+                    json_obj: dict[str, str] = json.loads(line)
+                    if stream := json_obj.get('stream'):
+                        reporter.log_output(stream.strip())
 
 
 class BuildDockerImages:
@@ -72,10 +157,10 @@ class BuildDockerImages:
         self._builder = _ImageBuilder(build_folder, registry, tag)
 
     def run(self, service_group: ServiceGroupDefinition) -> None:
-        import time
-        for service in track(service_group, 'Building...'):
-            self._builder.build(service)
-            time.sleep(1)
+        status_reporter = _OverallProcessingStatus(service_group)
+
+        for service in status_reporter.services:
+            self._builder.build(service, status_reporter.get_build_status_reporter())
 
 
 class ImageTarget(Target):
