58fdcbe03fc251792e11ccbee749429508cc0bf5
Richard Rzeszutek <richengguy@users.noreply.github.com>
2023-09-10 19:19:44 -0400

    Repo creation now also configures it.

--- src/gantry/forge/gitea.py
+++ src/gantry/forge/gitea.py
@@ -1,5 +1,4 @@
 from dataclasses import asdict, dataclass
-import json
 from typing import cast, TypedDict
 
 from .client import ForgeClient
@@ -35,6 +34,25 @@ class _CreateRepoRequest:
     default_branch: str = 'main'
 
 
+@dataclass
+class _EditRepoRequest:
+    has_actions: bool
+    has_issues: bool
+    has_packages: bool
+    has_projects: bool
+    has_pull_requests: bool
+    has_wiki: bool
+
+    allow_manual_merge: bool = False
+    allow_merge_commits: bool = False
+    allow_rebase: bool = False
+    allow_rebase_update: bool = True
+    allow_squash_merge: bool = True
+    default_delete_branch_after_merge: bool = True
+    default_merge_style: str = 'squash'
+    description: str | None = None
+
+
 class GiteaClient(ForgeClient):
     '''Push service images and definitions to a Gitea repo.'''
     API_BASE_URL = '/api/v1/'
@@ -46,23 +64,47 @@ class GiteaClient(ForgeClient):
     def api_base_url(self) -> str:
         return self.API_BASE_URL
 
-    def create_repo(self, name: str) -> None:
-        req = _CreateRepoRequest(name, 'Gantry-managed Repo')
-        _logger.debug('Request body: %s', asdict(req))
+    def clone_repo(self, name: str) -> None:
+        ...
+
+    def create_repo(self, name: str, desc: str | None = None) -> None:
+        if desc is None:
+            desc = 'Gantry-managed Repo'
+
+        # Create the repo.
+        req_create = _CreateRepoRequest(name, desc)
 
         resp = self.send_http_request('POST',
                                       self._org_repos_endpoint,
-                                      json=asdict(req),
-                                      success=set([201, 400]))
+                                      json=asdict(req_create),
+                                      success=set([201, 400, 409]))
 
-        if resp.status == 400:
-            contents = resp.json()
-            _logger.error('URL: %s', contents['url'])
-            _logger.error('Message: %s', contents['message'])
-            raise ForgeApiOperationFailed(self.provider_name(), 'Request failed; see debug log.')
+        match resp.status:
+            case 400:
+                contents = resp.json()
+                _logger.error('URL: %s', contents['url'])
+                _logger.error('Message: %s', contents['message'])
+                raise ForgeApiOperationFailed(self.provider_name(),
+                                              'Request failed; see debug log.')
+            case 409:
+                raise ForgeApiOperationFailed(
+                    self.provider_name(),
+                    f'The \'{self.owner_account}/{name}\' repo already exists.')
 
         repos = cast(_Repository, resp.json())
         _logger.debug('New repository created at %s.', repos['clone_url'])
+
+        # Now perform some basic configuration.
+        req_edit = _EditRepoRequest(has_actions=True,
+                                    has_issues=False,
+                                    has_packages=False,
+                                    has_projects=False,
+                                    has_pull_requests=True,
+                                    has_wiki=False)
+
+        resp = self.send_http_request('PATCH', self._repos_endpoint(name), json=asdict(req_edit))
+        repos = cast(_Repository, resp.json())
+        _logger.debug('Updated repo properties for %s.', repos['full_name'])
 
     def get_server_version(self) -> str:
         resp = self.send_http_request('GET', self._version_endpoint)
@@ -85,9 +127,12 @@ class GiteaClient(ForgeClient):
         return 'gitea'
 
     @property
-    def _version_endpoint(self) -> str:
-        return 'version'
-
-    @property
     def _org_repos_endpoint(self) -> str:
         return f'orgs/{self.owner_account}/repos'
+
+    def _repos_endpoint(self, name: str) -> str:
+        return f'repos/{self.owner_account}/{name}'
+
+    @property
+    def _version_endpoint(self) -> str:
+        return 'version'
