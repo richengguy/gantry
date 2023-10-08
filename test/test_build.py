8b6f72eaa466e904c3a572d47378d7e3e45de7f7
Richard Rzeszutek <richengguy@users.noreply.github.com>
2023-10-08 00:02:12 -0400

    Updated tests.

--- test/test_build.py
+++ test/test_build.py
@@ -21,8 +21,9 @@ def test_manifest_generation(samples_folder: Path, tmp_path: Path) -> None:
 
     runner = CliRunner()
     with runner.isolated_filesystem(temp_dir=tmp_path) as td:
-        result = runner.invoke(cli.main,
-                               ['build', '--tag', '123', '-X', 'skip-build', 'image', str(path)])
+        args = ['build', '--tag', '123', '-X', 'skip-build', 'image', str(path)]
+        result = runner.invoke(cli.main, args)
+        print(result.stdout)
         assert result.exit_code == 0
 
         manifest_file = Path(td) / 'build' / 'services.image' / MANIFEST_FILE
@@ -37,3 +38,54 @@ def test_manifest_generation(samples_folder: Path, tmp_path: Path) -> None:
 
         assert images[1].image == 'with-args:123'
         assert images[1].source == Path('build-args/with-args/Dockerfile')
+
+
+def test_multi_service_group_compose(samples_folder: Path, tmp_path) -> None:
+    group1 = samples_folder / 'service-definition' / 'build-args'
+    group2 = samples_folder / 'service-definition' / 'entrypoints'
+
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=tmp_path) as td:
+        args = ['build', '--tag', '123', 'compose', str(group1), str(group2)]
+        result = runner.invoke(cli.main, args)
+        print(result.stdout)
+        assert result.exit_code == 0
+
+        manifest_json = Path(td) / 'build' / 'services.compose' / MANIFEST_FILE
+        manifest = BuildManifest.load(manifest_json)
+
+        assert manifest.name == 'services-compose'
+        assert manifest.num_entries() == 2
+
+        entries = list(manifest.docker_compose_entries())
+        assert entries[0].compose_file == Path('build-args/docker-compose.yml')
+        assert entries[1].compose_file == Path('entrypoints/docker-compose.yml')
+
+        assert (Path(td) / 'build' / 'services.compose' / 'build-args').exists()
+        assert (Path(td) / 'build' / 'services.compose' / 'entrypoints').exists()
+
+
+def test_multi_service_group_image(samples_folder: Path, tmp_path) -> None:
+    group1 = samples_folder / 'service-definition' / 'build-args'
+    group2 = samples_folder / 'service-definition' / 'entrypoints'
+
+    runner = CliRunner()
+    with runner.isolated_filesystem(temp_dir=tmp_path) as td:
+        args = ['build', '--tag', '123', '-X', 'skip-build', 'image', str(group1), str(group2)]
+        result = runner.invoke(cli.main, args)
+        print(result.stdout)
+        assert result.exit_code == 0
+
+        manifest_json = Path(td) / 'build' / 'services.image' / MANIFEST_FILE
+        manifest = BuildManifest.load(manifest_json)
+
+        assert manifest.name == 'services-compose'
+        assert manifest.num_entries() == 5
+
+        images = list(manifest.image_entries())
+        assert images[0].image == 'no-args:123'
+        assert images[1].image == 'with-args:123'
+
+        assert images[2].image == 'complex-entrypoint:123'
+        assert images[3].image == 'default:123'
+        assert images[4].image == 'string-entrypoint:123'
