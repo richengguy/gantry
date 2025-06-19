2e4e0f418a1b4fbacbbdd6008b27ae1c34c381d5
Richard Rzeszutek <richengguy@users.noreply.github.com>
2025-06-18 20:23:44 -0400

    Added a py.typed file.

--- src/gantry/docker.py
+++ src/gantry/docker.py
@@ -265,6 +265,7 @@ class Docker:
             if socket is None:
                 raise DockerConnectionError()
             url = socket
+            print(url)
 
         try:
             _logger.debug("Create low-level Docker API client.")
